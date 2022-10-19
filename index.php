01b1d11a5a24039d66912cb6a1042b4cc26eabf0
T.pra <mossloveml5@gmail.com>
2022-10-19 12:03:56 +0700

    PHP include bootstrap

--- index.php
+++ index.php
@@ -8,34 +8,47 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Webboard</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </head>
 <?php
     if(!isset($_SESSION['id'])){
 ?>
 <body>
+    <div class="container">
     <h1 style="text-align: center;">Webboard</h1>
-    <hr>    
-    หมวดหมู่:
-        <select name="category">
-            <option value="all">--ทั้งหมด--</option>
-            <option value="general">--เรื่องทั่วไป--</option>
-            <option value="study">--เรื่องเรียน--</option>
-    </select>
-    <a href="login.php" style="float: right">เข้าสู่ระบบ</a>
+    <?php include "nav.php"; ?>
     <br>
-    <form action="post.php" method="get">
-    <?php 
-        for($i=1;$i<=10;$i++){
-            echo "<li><a href='post.php?id=$i'>กระทู้ $i</a></li>";
-        }
-    ?>
-    </form>
+    <div class="d-flex">
+        <div>
+            <label>หมวดหมู่</label>
+            <span class="btn btn-light dropdown-toggle btn-sm" type="button" id="button2" data-bs-toggle="dropdown" aria-expanded="false">--ทั้งหมด--</span>
+            <ul class="dropdown-menu" aria-labelledly="flase">
+                <li><a href="#" class="dropdown-item">ทั้งหมด</a></li>
+                <li><a href="#" class="dropdown-item">เรื่องเรียน</a></li>
+                <li><a href="#" class="dropdown-item">เรื่องทั่วไป</a></li>
+            </ul>
+        </div>
+    </div>
+    <table class="table table-striped">
+        <?php 
+            for($i=1;$i<=10;$i++){
+                echo "<tr><td><a href=post.php?id=$i style=text-decoration:none>กระทู้ $i </a></td></tr>";
+            }
+        ?>
+    </table>
+    </div>
 </body>
 <?php
     }else{
 ?>
 <body>
+    <div class="container">
     <h1 style="text-align: center;">Webboard</h1>
+    <?php include "nav.php"; ?>
     <hr>
     หมวดหมู่:
         <select name="category">
@@ -64,6 +77,7 @@
         }
     ?>
     </form>
+    </div>
 </body>
 <?php
     }
